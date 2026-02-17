ab997eb9648cb6be4843ab58938cc2e95cdc47d3
Arnout Engelen <arnout@bzzt.net>
2026-02-17 23:27:36 +0100

    Fix autorotate (on GNOME)
    *ideally* instead of relying on udev we'd convince mutter
    to orient the display as 'normal' instead of '90' by default,
    but I couldn't figure this out for now.

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
         modules = [
           rockchip.nixosModules.sdImageRockchip
           rockchip.nixosModules.dtOverlayPCIeFix
-          ({ pkgs, lib, ... }: {
+          ({ pkgs, lib, config, ... }: {
             # Ensure we don't try building zfs modules for the kernel (they're broken)
             nixpkgs.overlays = [
               (final: super: {
@@ -53,6 +53,7 @@
               inherit initialPassword;
               isNormalUser = true;
               extraGroups = [ "wheel" "networkmanager" ];
+              uid = 1000;
             };
 
             boot.kernelParams = [ "console=ttyS2,1500000n8" "rootwait" "root=LABEL=NIXOS_SD" "rw" ];
@@ -96,11 +97,26 @@
             security.rtkit.enable = true;
 
             # https://github.com/systemd/systemd/pull/35304#issuecomment-3855146191
-            services.udev.extraHwdb = ''
-              sensor:modalias:*sc7a20:*
-                ACCEL_MOUNT_MATRIX=0, 0, -1; 1, 0, 0; 0, 1, 0
-            '';
-
+            services.udev = {
+              extraHwdb = ''
+                sensor:modalias:*sc7a20:*
+                  ACCEL_MOUNT_MATRIX=1, 0, 0; 0, 0, 1; 0, 1, 0
+              '';
+              extraRules = ''
+                ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="1018", ATTRS{idProduct}=="1006", ENV{SYSTEMD_WANTS}+="landscape.service", TAG+="systemd"
+              '';
+            };
+            systemd.services.landscape = {
+              script = ''
+                ${pkgs.mutter}/bin/gdctl set --logical-monitor --primary --monitor=DSI-1 --transform normal
+                echo $?
+              '';
+              serviceConfig.User = username;
+              serviceConfig.Type = "oneshot";
+              environment = {
+                "DBUS_SESSION_BUS_ADDRESS" = "unix:path=/run/user/${toString config.users.users."${username}".uid}/bus";
+              };
+            };
             environment.systemPackages = with pkgs; [
               firefox
               gnomeExtensions.arc-menu
