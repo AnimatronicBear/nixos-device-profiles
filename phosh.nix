bc06f38007fa454fe6cdad170a25e0e849980d1d
Arnout Engelen <arnout@bzzt.net>
2026-03-14 13:18:13 +0100

    configure rotation for phosh

--- phosh.nix
+++ phosh.nix
@@ -12,6 +12,19 @@ in
     user = username;
     phocConfig.xwayland = "immediate";
   };
+
+  # https://github.com/systemd/systemd/pull/35304#issuecomment-3855146191
+  # gnome autorotate expects 'normal' is the _display panel_ normal, but
+  # mutter autoconfiguration rotates by 90deg.
+  # compensating with gdctl for now, though it would be better to 'properly'
+  # fix this.
+  services.udev = {
+    extraHwdb = ''
+      sensor:modalias:*sc7a20:*
+        ACCEL_MOUNT_MATRIX=1, 0, 0; 0, 0, 1; 0, 1, 0
+    '';
+  };
+
   # otherwise 
   # Mar 13 18:41:09 PineTab2 (phosh-session)[1135]: PAM unable to dlopen(/nix/store/751j4vwz7vbljv4h8d6rhw2dbwilkjk3-util-linux-2.41.3-lastlog/lib/security/pam_lastlog2.so): /nix/store/751j4vwz7vbljv4h8d6rhw2dbwilkjk3-util-linux-2.41.3-lastlog/lib/security/pam_lastlog2.so: undefined symbol: pam_syslog
   security.pam.services.login = {
