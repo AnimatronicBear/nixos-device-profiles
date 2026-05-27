562da34c93a9a9efa530bc92e3ab62830661b02c
aBear <animatronicbear@conversationsinspace.com>
2026-05-26 23:06:37 -0400

    Add native x86_64 installer ISO builds to flake
    Add installerConfigX86 function that builds natively for x86_64-linux
    using nixpkgs' iso-image module (no Rockchip dependencies).
    
    Three new packages:
      image-installer-x86pc       - console-only installer ISO
      image-installer-x86pc-gnome - with GNOME desktop
      image-installer-x86pc-plasma - with Plasma 6 desktop

--- flake.nix
+++ flake.nix
@@ -15,81 +15,143 @@
     };
   };
 
-  outputs = { self, nixpkgs, rockchip, utils, home-manager, ... }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rockchip,
+      utils,
+      home-manager,
+      ...
+    }:
 
-  let
-    settingsFile = if builtins.pathExists ./settings.nix then ./settings.nix else ./settings.nix.example;
-    settings = import settingsFile;
-    overlayModule = { config, lib, ... }: {
-      nixpkgs.overlays = import ./overlays;
-    };
+    let
+      settingsFile =
+        if builtins.pathExists ./settings.nix then ./settings.nix else ./settings.nix.example;
+      settings = import settingsFile;
+      overlayModule =
+        { config, lib, ... }:
+        {
+          nixpkgs.overlays = import ./overlays;
+        };
 
-    osConfig = buildPlatform: deviceModule: variant:
-      nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        specialArgs = { inherit rockchip buildPlatform settings; };
-        modules = [
-          overlayModule
-          { nixpkgs.hostPlatform = "aarch64-linux"; }
-          { nixpkgs.buildPlatform = buildPlatform; }
-          rockchip.nixosModules.sdImageRockchip
-          rockchip.nixosModules.noZFS
-          home-manager.nixosModules.home-manager
-          ./config.nix
-          variant
-          deviceModule
+      osConfig =
+        buildPlatform: deviceModule: variant:
+        nixpkgs.lib.nixosSystem {
+          system = "aarch64-linux";
+          specialArgs = { inherit rockchip buildPlatform settings; };
+          modules = [
+            overlayModule
+            { nixpkgs.hostPlatform = "aarch64-linux"; }
+            { nixpkgs.buildPlatform = buildPlatform; }
+            rockchip.nixosModules.sdImageRockchip
+            rockchip.nixosModules.noZFS
+            home-manager.nixosModules.home-manager
+            ./config.nix
+            variant
+            deviceModule
+          ];
+        };
+
+      installerConfig =
+        buildPlatform: deviceModule:
+        nixpkgs.lib.nixosSystem {
+          system = "aarch64-linux";
+          specialArgs = { inherit rockchip buildPlatform settings; };
+          modules = [
+            overlayModule
+            { nixpkgs.hostPlatform = "aarch64-linux"; }
+            { nixpkgs.buildPlatform = buildPlatform; }
+            rockchip.nixosModules.sdImageRockchipInstaller
+            rockchip.nixosModules.noZFS
+            home-manager.nixosModules.home-manager
+            ./config.nix
+            deviceModule
+          ];
+        };
+
+      installerConfigX86 =
+        variant:
+        nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit settings; };
+          modules = [
+            overlayModule
+            { nixpkgs.hostPlatform = "x86_64-linux"; }
+            home-manager.nixosModules.home-manager
+            ./config.nix
+            ./devices/x86pc.nix
+            (
+              { modulesPath, ... }:
+              {
+                imports = [ "${modulesPath}/installer/cd-dvd/iso-image.nix" ];
+              }
+            )
+            variant
+          ];
+        };
+    in
+    {
+      nixConfig = {
+        max-jobs = 4;
+        cores = 0;
+        extra-substituters = [ "https://nabam-nixos-rockchip.cachix.org" ];
+        extra-trusted-public-keys = [
+          "nabam-nixos-rockchip.cachix.org-1:BQDltcnV8GS/G86tdvjLwLFz1WeFqSk7O9yl+DR0AVM"
         ];
       };
 
-    installerConfig = buildPlatform: deviceModule:
-      nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        specialArgs = { inherit rockchip buildPlatform settings; };
-        modules = [
-          overlayModule
-          { nixpkgs.hostPlatform = "aarch64-linux"; }
-          { nixpkgs.buildPlatform = buildPlatform; }
-          rockchip.nixosModules.sdImageRockchipInstaller
-          rockchip.nixosModules.noZFS
-          home-manager.nixosModules.home-manager
-          ./config.nix
-          deviceModule
-        ];
-      };
-  in
-  {
-    nixConfig = {
-      max-jobs = 4;
-      cores = 0;
-      extra-substituters = [ "https://nabam-nixos-rockchip.cachix.org" ];
-      extra-trusted-public-keys = [
-        "nabam-nixos-rockchip.cachix.org-1:BQDltcnV8GS/G86tdvjLwLFz1WeFqSk7O9yl+DR0AVM"
-      ];
-    };
+      nixosConfigurations.PineBookPro = osConfig "x86_64-linux" ./devices/pinebook-pro.nix ./gnome.nix;
+      nixosConfigurations.PineBookPro-plasma =
+        osConfig "x86_64-linux" ./devices/pinebook-pro.nix
+          ./plasma.nix;
+      nixosConfigurations.PineBookPro-phosh =
+        osConfig "x86_64-linux" ./devices/pinebook-pro.nix
+          ./phosh.nix;
+      nixosConfigurations.PineTab2 = osConfig "x86_64-linux" ./devices/pinetab2.nix ./gnome.nix;
+      nixosConfigurations.PineTab2-plasma = osConfig "x86_64-linux" ./devices/pinetab2.nix ./plasma.nix;
+      nixosConfigurations.PineTab2-phosh = osConfig "x86_64-linux" ./devices/pinetab2.nix ./phosh.nix;
+    }
+    // utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = nixpkgs.lib;
+        checks = import ./checks.nix {
+          inherit
+            lib
+            pkgs
+            system
+            osConfig
+            installerConfigX86
+            ;
+        };
+      in
+      {
+        packages.image-gnome =
+          (osConfig system ./devices/pinebook-pro.nix ./gnome.nix).config.system.build.sdImage;
+        packages.image-plasma =
+          (osConfig system ./devices/pinebook-pro.nix ./plasma.nix).config.system.build.sdImage;
+        packages.image-pinetab2-gnome =
+          (osConfig system ./devices/pinetab2.nix ./gnome.nix).config.system.build.sdImage;
+        packages.image-pinetab2-plasma =
+          (osConfig system ./devices/pinetab2.nix ./plasma.nix).config.system.build.sdImage;
+        packages.uboot = (osConfig system ./devices/pinebook-pro.nix { }).config.rockchip.uBoot;
+        packages.uboot-pinetab2 = (osConfig system ./devices/pinetab2.nix { }).config.rockchip.uBoot;
+        packages.image-installer-pinebookpro =
+          (installerConfig system ./devices/pinebook-pro.nix).config.system.build.sdImage;
+        packages.image-installer-pinetab2 =
+          (installerConfig system ./devices/pinetab2.nix).config.system.build.sdImage;
+        packages.image-installer-x86pc = (installerConfigX86 { }).config.system.build.isoImage;
+        packages.image-installer-x86pc-gnome =
+          (installerConfigX86 ./gnome.nix).config.system.build.isoImage;
+        packages.image-installer-x86pc-plasma =
+          (installerConfigX86 ./plasma.nix).config.system.build.isoImage;
+        packages.default = self.packages.${system}.image-gnome;
 
-    nixosConfigurations.PineBookPro = osConfig "x86_64-linux" ./devices/pinebook-pro.nix ./gnome.nix;
-    nixosConfigurations.PineBookPro-plasma = osConfig "x86_64-linux" ./devices/pinebook-pro.nix ./plasma.nix;
-    nixosConfigurations.PineBookPro-phosh = osConfig "x86_64-linux" ./devices/pinebook-pro.nix ./phosh.nix;
-    nixosConfigurations.PineTab2 = osConfig "x86_64-linux" ./devices/pinetab2.nix ./gnome.nix;
-    nixosConfigurations.PineTab2-plasma = osConfig "x86_64-linux" ./devices/pinetab2.nix ./plasma.nix;
-    nixosConfigurations.PineTab2-phosh = osConfig "x86_64-linux" ./devices/pinetab2.nix ./phosh.nix;
-  } // utils.lib.eachDefaultSystem (system: let
-    pkgs = nixpkgs.legacyPackages.${system};
-    lib = nixpkgs.lib;
-    checks = import ./checks.nix { inherit lib pkgs system osConfig; };
-  in {
-    packages.image-gnome = (osConfig system ./devices/pinebook-pro.nix ./gnome.nix).config.system.build.sdImage;
-    packages.image-plasma = (osConfig system ./devices/pinebook-pro.nix ./plasma.nix).config.system.build.sdImage;
-    packages.image-pinetab2-gnome = (osConfig system ./devices/pinetab2.nix ./gnome.nix).config.system.build.sdImage;
-    packages.image-pinetab2-plasma = (osConfig system ./devices/pinetab2.nix ./plasma.nix).config.system.build.sdImage;
-    packages.uboot = (osConfig system ./devices/pinebook-pro.nix { }).config.rockchip.uBoot;
-    packages.uboot-pinetab2 = (osConfig system ./devices/pinetab2.nix { }).config.rockchip.uBoot;
-    packages.image-installer-pinebookpro = (installerConfig system ./devices/pinebook-pro.nix).config.system.build.sdImage;
-    packages.image-installer-pinetab2 = (installerConfig system ./devices/pinetab2.nix).config.system.build.sdImage;
-    packages.default = self.packages.${system}.image-gnome;
+        formatter = pkgs.nixfmt-tree;
 
-    formatter = pkgs.nixfmt-tree;
-
-    checks = checks;
-  });
+        checks = checks;
+      }
+    );
 }
